d9be7bdacff8cfc613a49c540ecc3ee6ca646a91
Egil Bukhave Edström <ege001@edu.zealand.dk>
2026-04-13 10:30:19 +0200

    Controller Record

---
+++ MusicCollectionREST/Controllers/RecordController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicCollectionREST.Repos;
+using MusicCollectionREST.Models;
+
+namespace MusicCollectionREST.Controllers
+{
+    [Route("api/[controller]")]
+    public class RecordController : Controller
+    {
+        private readonly IRecordRepo _recordRepo;
+
+        public RecordController(IRecordRepo recordRepo)
+        {
+            _recordRepo = recordRepo;
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            IEnumerable<Record>? result = _recordRepo.GetAll();
+            if (result == null || !result.Any())
+            {
+                return NoContent();
+            }
+            return Ok(result);
+        }
+
+    }
+}
