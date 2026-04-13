016334af5e1f686412b20a3a17f0931fb64436fb
Egil Bukhave Edström <ege001@edu.zealand.dk>
2026-04-13 10:36:15 +0200

    CORS

--- MusicCollectionREST/Program.cs
+++ MusicCollectionREST/Program.cs
@@ -1,6 +1,19 @@
+using MusicCollectionREST.Repos;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+        });
+});
+
+builder.Services.AddSingleton<IRecordRepo, RecordRepo>();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -9,11 +22,12 @@ builder.Services.AddOpenApi();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.MapOpenApi();
+//}
 
+//app.UseCors("DefaultPolicy");
 app.UseAuthorization();
 
 app.MapControllers();
