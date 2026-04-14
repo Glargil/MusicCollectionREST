4120f0da51a7fabf0f86dd1b12d5093d1aa948c2
thecorf <mim010@edu.zealand.dk>
2026-04-14 09:30:17 +0200

    Login

--- MusicCollectionREST/Program.cs
+++ MusicCollectionREST/Program.cs
@@ -1,5 +1,21 @@
+using Microsoft.OpenApi.Models;
 using MusicCollectionREST.Repos;
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSwaggerGen(opt =>
+{
+    opt.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+
+    opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter token as: Bearer {your_token}",
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey, // Vi bruger ApiKey for at kunne skrive 'Bearer ' manuelt
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+});
 
 // Add services to the container.
 builder.Services.AddCors(options =>
@@ -19,6 +35,9 @@ builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // Configure the HTTP request pipeline.
 //if (app.Environment.IsDevelopment())
