6ea98fa871c0f23e0a7f0748b6be5685019a9902
Egil Bukhave Edström <ege001@edu.zealand.dk>
2026-04-14 09:12:11 +0200

    added another pink floyd song

--- MusicCollectionREST/Repos/RecordRepo.cs
+++ MusicCollectionREST/Repos/RecordRepo.cs
@@ -6,6 +6,7 @@ namespace MusicCollectionREST.Repos
         private int _nextId = 1;
         private List<Record> _record = new List<Record> {
              new Record("The Dark Side of the Moon", "Pink Floyd", 163, 1973),
+             new Record("Another Brick in The Wall", "Pink Floyd", 161, 1979),
              new Record("Abbey Road", "The Beatles", 167, 1969),
              new Record("Thriller", "Michael Jackson", 162, 1982)
             };
